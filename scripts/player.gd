6d25ebb5d460506780911fcb902e6d01b2225f79
Jon Howard <johnny.rtn@gmail.com>
2024-09-08 14:56:19 -0230

    progress on projectile

--- scripts/player.gd
+++ scripts/player.gd
@@ -5,6 +5,11 @@ signal hit
 @export var speed = 400 
 var screen_size
 
+var projectile_speed = 600
+var projectile_scene = preload("res://scenes/projectile.tscn")
+
+@onready var projectile_container = $ProjectileContainer
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -15,6 +20,10 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	var velocity = Vector2.ZERO # The player's movement vector.
+	
+	if Input.is_action_just_pressed("shoot_projectile"):
+		shoot_projectile(velocity.x < 0)
+
 	if Input.is_action_pressed("move_right"):
 		velocity.x += 1
 	if Input.is_action_pressed("move_left"):
@@ -41,6 +50,20 @@ func _process(delta: float) -> void:
 		$AnimatedSprite2D.animation  = "up"
 		$AnimatedSprite2D.flip_v = velocity.y > 0
 
+
+func _physics_process(delta: float) -> void:
+	var screen_size = get_viewport_rect().size
+	global_position = global_position.clamp(Vector2(0,0), screen_size)
+
+func shoot_projectile(forward: bool) -> void:
+	var projectile_instance = projectile_scene.instantiate()
+	projectile_container.add_child(projectile_instance)
+	projectile_instance.global_position = global_position
+	if forward:
+		projectile_instance.global_position.x += 80
+	else:
+		projectile_instance.global_position.x -= 80
+	# play projectile sound here
 
 func _on_body_entered(body: Node2D) -> void:
 	Global.camera.shake(0.2, 6)
